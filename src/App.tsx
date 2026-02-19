592d7ea492d4fba12ee7f615ddb7d92557241323
Arcee Luceno <lucenoarcee0@gmail.com>
2026-02-19 10:23:57 +0800

    Refactor App component layout and update header title

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ export default function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100 p-6">
       <div className="max-w-6xl mx-auto">
+        <div className="justify-center flex">
         <header className="mb-6">
-          <h1 className="text-2xl sm:text-3xl font-semibold">
-            RC lazy project
+          <h1 className="text-2xl sm:text-3xl font-semibold ">
+            Boys at the Back
           </h1>
-          <p className="text-slate-400 mt-1">
-            Everything is based on Asia/Manila time.
-          </p>
         </header>
+        </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           <PhilippineClock />
           <AwayTimer30s />
         </div>
       </div>
+      <footer className="justify-center flex">
+        <div className="max-w-6xl mx-auto mt-12 text-sm text-slate-500">
+          all rights reserved © RC 2026.
+           </div>
+      </footer>
     </div>
   );
 }
